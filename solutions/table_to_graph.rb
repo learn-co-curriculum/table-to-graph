61df59fc75b7c422c78a3a3e36a9b64bb87bd699
Rupa <rupa211984@gmail.com>
2021-01-25 15:18:26 -0800

    Add explanation

--- solutions/table_to_graph.rb
+++ solutions/table_to_graph.rb
@@ -95,5 +95,61 @@ if __FILE__ == $PROGRAM_NAME
   puts
 end
 
+# def table_to_graph(friends)
+#   data_string = friends[55..-19].gsub(/<\/tr><tr>/, "")
+#   data = data_string.split("</td><td>")
+#   graph = Hash.new { |h, k| h[k] = [] }
+#   idx = 0
+
+#   while idx < data.length
+#     friends_list = (data[idx + 1] || "").split(", ")
+#     vertex = data[idx]
+#     graph[vertex] = friends_list
+
+#     friends_list.each do |friend| 
+#       graph[friend] << vertex unless graph[friend].include?(vertex)
+#     end
+
+#     idx += 2
+#   end
+
+#   graph
+# end
+
 # Please add your pseudocode to this file
+#########################################################################################
+# initialize a variable, data_string, and store the parts of the string between the TDs
+# initialize data and split data_string's cells into an array, so that the left cell always
+#     comes just before the right cell
+# initialize a Hash/Object called graph
+#
+# iterate over every other element of data:
+#   store the friends in an array (friends are the next element in data)
+#   add the current friend as a key in the graph
+#   set the value to the friends array
+#   set each friend as a key in the graph and add the current friend to their adjacency lists
+#     skipping duplicates
+#
+# return graph
+#########################################################################################
+
 # And a written explanation of your solution
+#########################################################################################
+# From the prompt I know that there will always be at least one friend in the table. However,
+# there is no guarantee that that friend will have any friends :(  I also know that the 
+# table will always have the same HTML markup, just with a different number of rows.
+#
+# I figured that I should first split the table into an array, so that each element of the
+# array would hold the friend data in a specific order. For example, for a table with only
+# two cells, my array would look like this: ["Jam", "Gran, Minnie, Tabitha"] or [left cell, right cell]
+# Now I can iterate over the array and know that every other element is an adjacency list
+# for the friend in the element just before it.
+#
+# When I iterate over the array, I increase the index by 2 each time because of how my
+# array is ordered. I set the currently iterated over element as the key in the graph,
+# and then split the next element into an array of strings, which then becomes the value
+# of the key in the graph (the adjacency list). I then need all of those friends in the 
+# adjacency list to point back to the original friend/key, but I have to ensure that if 
+# that friend is already a key in the graph that I don't duplicate any data in their adjacency 
+# list.
+#########################################################################################
